b03e1a9601a454ffcc887cf9f07eb07c940faceb
Rory Hiscock <rory.hiscock@hotmail.com>
2018-10-11 08:24:39 +1000

    Add files via upload

---
+++ Scripts/ScreenRes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRes : MonoBehaviour {
+
+    void Start()
+    {
+        int width = 1200; // or something else
+        int height = 600; // or something else
+        bool isFullScreen = false; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
+
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+    }
+}
